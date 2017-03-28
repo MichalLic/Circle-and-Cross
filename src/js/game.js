fa5811c1a4d9de3f5ff24bdb8841552e31abf26b
Michal <m14lickiewicz@gmail.com>
2017-03-28 22:25:32 +0200

    Set function to show sign in game fields.

--- src/js/game.js
+++ src/js/game.js
@@ -3,42 +3,55 @@ var GameApp = {
     //variables
 
     //init
-    init: function (){
+    init: function () {
         GameApp.setIdenticalHeight(GameApp.$GAME_FIELD);
         GameApp.onChoose();
     },
 
     //functions
     onChoose: function () {
-        GameApp.$GAME_FIELD.on('click', function(e){
+        GameApp.$GAME_FIELD.on('click', function (e) {
             e.preventDefault();
             console.log(this);
-            GameApp.assignChar(this);
-        });
+            if ($(this).hasClass('empty')) {
+                $(this).addClass('circle').removeClass('empty').siblings().addClass('next-cross').removeClass('empty');
+                $(this).css('pointer-events', 'none');
+            }
+            else if ($(this).hasClass('next-cross')) {
+                $(this).addClass('cross').siblings().addClass('next-circle').removeClass('next-cross');
+                $(this).css('pointer-events', 'none');
+            }
+            else {
+                $(this).addClass('circle').siblings().addClass('next-cross');
+                $(this).css('pointer-events', 'none');
+            }
+
+            //GameApp.assignChar(this);
+        })
     },
+
     assignChar: function (gameField) {
         /*@see https://codepen.io/anon/pen/BWVYqV?editors=1100*/
         /*https://codepen.io/JTParrett/pen/pDJAf*/
         GameApp.show(gameField)
     },
 
-    show: function (gameField){
+    show: function (gameField) {
         $(gameField).addClass('show-circle');
     },
 
 
-    setIdenticalHeight: function(field){
+    setIdenticalHeight: function (field) {
         var widthField = field.width();
-        $(window).resize(function(){
-           field.height(widthField)
+        $(window).resize(function () {
+            field.height(widthField)
         });
         console.log(widthField);
         console.log(field.height(widthField));
     }
 
 
-
 };
-$(document).ready(function(){
+$(document).ready(function () {
     GameApp.init()
 });
